1a89aa4d6c0f895128ba15fd2023562aeb3db11e
dkjeong0815 <dkjeong0815@gmail.com>
2021-06-17 12:30:42 +0900

    Coffee Machine - 2 Material

---
+++ keyshot-configurator-imagemapping.js
@@ -0,0 +1,893 @@
+imagemappingdata =
+[
+    {
+        "image": "Web Coffee multimat.27.1.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "14+3"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.2.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "14+2"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.3.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "14+5"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.4.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "14+10"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.5.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "14+6"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.6.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "14+11"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.7.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "14+7"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.8.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "14+12"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.9.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "14+9"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.10.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "14+4"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.11.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+3",
+            "318+321"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.12.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+3",
+            "318+324"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.13.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+3",
+            "318+372"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.14.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+3",
+            "318+435"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.15.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+3",
+            "318+473"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.16.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+3",
+            "318+479"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.17.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+3",
+            "318+486"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.18.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+3",
+            "318+494"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.19.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+2",
+            "318+321"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.20.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+2",
+            "318+324"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.21.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+2",
+            "318+372"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.22.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+2",
+            "318+435"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.23.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+2",
+            "318+473"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.24.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+2",
+            "318+479"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.25.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+2",
+            "318+486"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.26.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+2",
+            "318+494"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.27.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+5",
+            "318+321"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.28.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+5",
+            "318+324"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.29.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+5",
+            "318+372"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.30.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+5",
+            "318+435"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.31.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+5",
+            "318+473"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.32.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+5",
+            "318+479"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.33.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+5",
+            "318+486"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.34.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+5",
+            "318+494"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.35.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+10",
+            "318+321"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.36.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+10",
+            "318+324"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.37.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+10",
+            "318+372"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.38.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+10",
+            "318+435"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.39.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+10",
+            "318+473"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.40.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+10",
+            "318+479"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.41.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+10",
+            "318+486"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.42.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+10",
+            "318+494"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.43.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+6",
+            "318+321"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.44.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+6",
+            "318+324"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.45.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+6",
+            "318+372"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.46.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+6",
+            "318+435"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.47.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+6",
+            "318+473"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.48.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+6",
+            "318+479"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.49.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+6",
+            "318+486"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.50.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+6",
+            "318+494"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.51.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+11",
+            "318+321"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.52.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+11",
+            "318+324"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.53.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+11",
+            "318+372"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.54.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+11",
+            "318+435"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.55.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+11",
+            "318+473"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.56.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+11",
+            "318+479"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.57.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+11",
+            "318+486"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.58.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+11",
+            "318+494"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.59.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+7",
+            "318+321"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.60.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+7",
+            "318+324"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.61.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+7",
+            "318+372"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.62.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+7",
+            "318+435"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.63.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+7",
+            "318+473"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.64.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+7",
+            "318+479"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.65.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+7",
+            "318+486"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.66.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+7",
+            "318+494"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.67.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+12",
+            "318+321"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.68.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+12",
+            "318+324"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.69.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+12",
+            "318+372"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.70.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+12",
+            "318+435"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.71.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+12",
+            "318+473"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.72.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+12",
+            "318+479"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.73.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+12",
+            "318+486"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.74.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+12",
+            "318+494"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.75.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+9",
+            "318+321"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.76.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+9",
+            "318+324"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.77.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+9",
+            "318+372"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.78.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+9",
+            "318+435"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.79.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+9",
+            "318+473"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.80.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+9",
+            "318+479"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.81.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+9",
+            "318+486"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.82.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+9",
+            "318+494"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.83.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+4",
+            "318+321"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.84.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+4",
+            "318+324"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.85.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+4",
+            "318+372"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.86.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+4",
+            "318+435"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.87.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+4",
+            "318+473"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.88.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+4",
+            "318+479"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.89.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+4",
+            "318+486"
+        ]
+    },
+    {
+        "image": "Web Coffee multimat.27.90.jpg",
+        "modelsets": [
+            550920302
+        ],
+        "multimaterials": [
+            "14+4",
+            "318+494"
+        ]
+    }
+];
---
+++ keyshot-configurator-modeldata.js
@@ -0,0 +1,164 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "icon": "%ea%b2%80.png",
+                    "multimaterials": [
+                        "14+3"
+                    ],
+                    "name": "검"
+                },
+                {
+                    "icon": "%eb%b9%9b%ea%b2%80.png",
+                    "multimaterials": [
+                        "14+2"
+                    ],
+                    "name": "빛검"
+                },
+                {
+                    "icon": "%ec%b4%88.png",
+                    "multimaterials": [
+                        "14+5"
+                    ],
+                    "name": "초"
+                },
+                {
+                    "icon": "%eb%b9%9b%ec%b4%88.png",
+                    "multimaterials": [
+                        "14+10"
+                    ],
+                    "name": "빛초"
+                },
+                {
+                    "icon": "%ed%9a%8c.png",
+                    "multimaterials": [
+                        "14+6"
+                    ],
+                    "name": "회"
+                },
+                {
+                    "icon": "%eb%b9%9b%ed%9a%8c.png",
+                    "multimaterials": [
+                        "14+11"
+                    ],
+                    "name": "빛회"
+                },
+                {
+                    "icon": "%eb%b9%a8.png",
+                    "multimaterials": [
+                        "14+7"
+                    ],
+                    "name": "빨"
+                },
+                {
+                    "checked": true,
+                    "icon": "%eb%b9%9b%eb%b9%a8.png",
+                    "multimaterials": [
+                        "14+12"
+                    ],
+                    "name": "빛빨"
+                },
+                {
+                    "icon": "%eb%b9%9b%ed%8c%8c.png",
+                    "multimaterials": [
+                        "14+9"
+                    ],
+                    "name": "빛파"
+                },
+                {
+                    "icon": "%ed%8c%8c.png",
+                    "multimaterials": [
+                        "14+4"
+                    ],
+                    "name": "파"
+                }
+            ],
+            "name": "색상"
+        },
+        {
+            "materials": [
+                {
+                    "checked": true,
+                    "icon": "%ed%95%98%ec%96%91.png",
+                    "multimaterials": [
+                        "318+321"
+                    ],
+                    "name": "하양"
+                },
+                {
+                    "icon": "%ea%b2%80%ec%a0%95.png",
+                    "multimaterials": [
+                        "318+324"
+                    ],
+                    "name": "검정"
+                },
+                {
+                    "icon": "%ed%9d%b0%20%ed%88%ac%eb%aa%85.png",
+                    "multimaterials": [
+                        "318+372"
+                    ],
+                    "name": "흰 투명"
+                },
+                {
+                    "icon": "%ea%b4%91%ed%95%99%20%ec%9c%a0%eb%a6%ac.png",
+                    "multimaterials": [
+                        "318+435"
+                    ],
+                    "name": "광학 유리"
+                },
+                {
+                    "icon": "%ec%b2%b4%ed%81%ac%20%eb%ac%b4%eb%8a%ac.png",
+                    "multimaterials": [
+                        "318+473"
+                    ],
+                    "name": "체크 무늬"
+                },
+                {
+                    "icon": "%eb%a0%8c%ec%a6%88.png",
+                    "multimaterials": [
+                        "318+479"
+                    ],
+                    "name": "렌즈"
+                },
+                {
+                    "icon": "%ec%97%91%ec%8a%a4%eb%a0%88%ec%9d%b4%20%ea%b2%80%ec%a0%95.png",
+                    "multimaterials": [
+                        "318+486"
+                    ],
+                    "name": "엑스레이 검정"
+                },
+                {
+                    "icon": "%ec%97%91%ec%8a%a4%eb%a0%88%ec%9d%b4%20%ed%95%98%ec%96%91.png",
+                    "multimaterials": [
+                        "318+494"
+                    ],
+                    "name": "엑스레이 하양"
+                }
+            ],
+            "name": "상단 색상"
+        }
+    ],
+    "products": [
+        {
+            "icon": "%ea%b8%b0%eb%b3%b8%20%ec%83%89.png",
+            "materialgroups": [
+                "색상"
+            ],
+            "modelset": 1004,
+            "name": "기본 색"
+        },
+        {
+            "checked": true,
+            "icon": "%ec%83%89%20%ec%a1%b0%ed%95%a9.png",
+            "materialgroups": [
+                "색상",
+                "상단 색상"
+            ],
+            "modelset": 550920302,
+            "name": "색 조합"
+        }
+    ],
+    "studios": []
+};
